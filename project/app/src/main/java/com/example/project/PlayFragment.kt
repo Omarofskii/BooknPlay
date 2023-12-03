f6e6fa06331209f7028d17e137deba3f4a9229fd
Shan Ahmad Saqib <shan.ahmadsaqib@student.ap.be>
2023-12-03 12:46:20 +0100

    play page

--- project/app/src/main/java/com/example/project/PlayFragment.kt
+++ project/app/src/main/java/com/example/project/PlayFragment.kt
@@ -1,10 +1,15 @@
 package com.example.project
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.project.databinding.ActivitySignUpBinding
+import com.example.project.databinding.ActivitySigninBinding
+import com.example.project.databinding.FragmentPlayBinding
 
 
 private const val ARG_PARAM1 = "param1"
@@ -12,16 +17,31 @@ private const val ARG_PARAM2 = "param2"
 
 
 class PlayFragment : Fragment() {
+    private lateinit var binding: FragmentPlayBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = FragmentPlayBinding.inflate(layoutInflater)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        binding.bookCourt.setOnClickListener(){
+            val intent = Intent(activity, BookCourtActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.playMatch.setOnClickListener(){
+            val intent = Intent(activity, PlayMatchActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
     override fun onCreateView(
