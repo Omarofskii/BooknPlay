6d2538220d733d4171da98978612e951907425a0
Shan Ahmad Saqib <shan.ahmadsaqib@student.ap.be>
2023-12-15 21:18:10 +0100

    Update Court.kt

--- project/app/src/main/java/com/example/project/Court.kt
+++ project/app/src/main/java/com/example/project/Court.kt
@@ -1,8 +1,21 @@
 package com.example.project
 
+import com.google.type.DateTime
+import com.google.firebase.Timestamp
+import java.util.Date
+
 data class Court(
     val id: String = "",
     val name: String = "",
     val availableTimes: List<String> = emptyList(),
     val imageUrl: String = ""
+)
+
+data class Match(
+    var id: String = "",
+    val courtId: String = "",
+    val dateTime: Timestamp = Timestamp.now(),
+    val organizerId: String = "",
+    val playerList: List<String> = listOf(),
+    val pricePerPlayer: Double = 0.0 // Make sure all fields have default values
 )
