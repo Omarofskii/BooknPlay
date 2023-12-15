d7f929c2d1f650fa7d802b6bcfd2ad7bb2f74d31
Shan Ahmad Saqib <shan.ahmadsaqib@student.ap.be>
2023-12-15 21:18:19 +0100

    Update PlayMatchActivity.kt

--- project/app/src/main/java/com/example/project/PlayMatchActivity.kt
+++ project/app/src/main/java/com/example/project/PlayMatchActivity.kt
@@ -1,11 +1,46 @@
 package com.example.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
-class PlayMatchActivity : AppCompatActivity() {
+class PlayMatchActivity : AppCompatActivity(), MatchInteractionListener {
+
+    private lateinit var recyclerView: RecyclerView
+    private val viewModel by viewModels<MatchesViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_match)
+
+        val buttonGoToCreateMatch: Button = findViewById(R.id.buttonGoToCreateMatch)
+        buttonGoToCreateMatch.setOnClickListener {
+            val intent = Intent(this, CreateMatchActivity::class.java)
+            startActivity(intent)
+        }
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView2).apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@PlayMatchActivity)
+            adapter = MatchAdapter(emptyList(), this@PlayMatchActivity)
+        }
+
+        viewModel.matches.observe(this, {matches ->
+            (recyclerView.adapter as MatchAdapter).updateMatches(matches)
+        })
+
+        viewModel.fetchMatches()
+    }
+
+    override fun onJoinMatchClicked(matchId: String) {
+        FirebaseAuth.getInstance().currentUser?.uid?.let { userId ->
+            viewModel.joinMatch(matchId, userId)
+        }
     }
 }
