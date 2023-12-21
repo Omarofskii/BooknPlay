a141448b4d8ea061635a03962c227b8a70143c75
Shan Ahmad Saqib <shan.ahmadsaqib@student.ap.be>
2023-12-21 02:00:28 +0100

    Update MatchAdapter.kt

--- project/app/src/main/java/com/example/project/MatchAdapter.kt
+++ project/app/src/main/java/com/example/project/MatchAdapter.kt
@@ -1,23 +1,21 @@
 package com.example.project
 
 
-import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObject
-import com.google.firebase.Timestamp
-import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
 import java.text.SimpleDateFormat
-import java.util.Date
 import java.util.Locale
-import java.util.logging.Handler
 
 interface MatchInteractionListener {
     fun onJoinMatchClicked(matchId: String)
@@ -33,13 +31,17 @@ class MatchAdapter(private var matches: List<Match>, private val listener: Match
 
     class MatchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-            val textViewCourtId: TextView = itemView.findViewById(R.id.Place)
+            val imageViewCourt: ImageView = itemView.findViewById(R.id.imageViewCourt)
+            val textViewCourtName: TextView = itemView.findViewById(R.id.textViewCourtName)
+
+
             val textViewDateTime: TextView = itemView.findViewById(R.id.Date)
             val textViewOrganizerName: TextView = itemView.findViewById(R.id.textViewOrganizerId)
             val joinButton: Button = itemView.findViewById(R.id.joinMatch)
             val textViewPlayerName1: TextView = itemView.findViewById(R.id.playerName1)
             val textViewPlayerName2: TextView = itemView.findViewById(R.id.playerName2)
             val textViewPlayerName3: TextView = itemView.findViewById(R.id.playerName3)
+            val textViewPricePerPlayer: TextView = itemView.findViewById(R.id.PriceView)
             // Bind other views in the item layout as needed
 
     }
@@ -74,15 +76,18 @@ class MatchAdapter(private var matches: List<Match>, private val listener: Match
 
     override fun onBindViewHolder(holder: MatchViewHolder, position: Int) {
         val match = matches[position]
-        holder.textViewCourtId.text = match.courtId
+
         holder.textViewDateTime.text = match.dateTime.toDate().let { date ->
             SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(date)
         }
 
 
-        holder.textViewPlayerName1.visibility = View.VISIBLE
-        holder.textViewPlayerName2.visibility = View.VISIBLE
-        holder.textViewPlayerName3.visibility = View.VISIBLE
+
+
+        val playerListSize = match.playerList.size
+        holder.textViewPlayerName1.visibility = if (playerListSize > 0) View.VISIBLE else View.GONE
+        holder.textViewPlayerName2.visibility = if (playerListSize > 1) View.VISIBLE else View.GONE
+        holder.textViewPlayerName3.visibility = if (playerListSize > 2) View.VISIBLE else View.GONE
 
 
         val organizerName = userNameCache[match.organizerId]
@@ -109,10 +114,43 @@ class MatchAdapter(private var matches: List<Match>, private val listener: Match
             Log.d("MatchAdapter", "Player at index $index: $playerId")
             when (index) {
 
-                1 -> displayPlayerName(playerId, holder.textViewPlayerName1)
+                0 -> displayPlayerName(playerId, holder.textViewPlayerName1)
+                1 -> displayPlayerName(playerId, holder.textViewPlayerName2)
                 2 -> displayPlayerName(playerId, holder.textViewPlayerName3)
             }
         }
+        val drawableMap = mapOf(
+            "ElCitedel" to R.drawable.court1,
+            "Court 1" to R.drawable.court2,
+            "luchtbal" to R.drawable.court3
+        )
+
+        db.collection("courts").whereEqualTo("id", match.courtId).get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    val courtDocument = querySnapshot.documents[0] // Take the first document that matches
+                    val court = courtDocument.toObject<Court>()
+                    court?.let {
+                        holder.textViewCourtName.text = it.name
+                        val imageResId = drawableMap[court.name] ?: R.drawable.court1 // Fallback to a default image if name not found
+                        holder.imageViewCourt.setImageResource(imageResId)
+                    }
+                } else {
+                    Log.d("MatchAdapter", "No such court document with id: ${match.courtId}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("MatchAdapter", "Error fetching court details", exception)
+            }
+
+        val totalCost = 10.0 // Total cost for the match
+        val numberOfPlayers = match.playerList.size + 1 // Ensures we don't divide by zero
+        val pricePerPlayer = totalCost / numberOfPlayers
+
+        // Format the price to 2 decimal places and set it to the TextView
+        val priceText = "prijs per speler: €" + String.format(Locale.getDefault(), "%.2f", pricePerPlayer)
+        holder.textViewPricePerPlayer.text = priceText
+
 
 
         holder.joinButton.setOnClickListener{
