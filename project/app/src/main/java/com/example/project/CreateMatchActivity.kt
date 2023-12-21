65664af9b8464a1e3a0c7b14b43aef2a6f2d6652
Shan Ahmad Saqib <shan.ahmadsaqib@student.ap.be>
2023-12-21 02:00:26 +0100

    Update CreateMatchActivity.kt

--- project/app/src/main/java/com/example/project/CreateMatchActivity.kt
+++ project/app/src/main/java/com/example/project/CreateMatchActivity.kt
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -18,107 +20,180 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 class CreateMatchActivity : AppCompatActivity() {
 
     private val auth: FirebaseAuth = Firebase.auth
     private val db: FirebaseFirestore = Firebase.firestore
 
-    private lateinit var spinnerCourt: Spinner
-    private lateinit var spinnerTime: Spinner
-    private lateinit var buttonSaveMatch: Button
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var createAdapter: CreateAdapter
+
+    val reservedTimesMap = mutableMapOf<String, List<String>>()
+
+
 
     private var courts: List<Court> = emptyList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_match)
 
-
-
-        spinnerCourt = findViewById(R.id.spinnerCourt)
-        spinnerTime = findViewById(R.id.spinnerTime)
-
-        setupCourtSpinner()
-
-        buttonSaveMatch = findViewById(R.id.buttonSaveMatch)
-        buttonSaveMatch.setOnClickListener {
-            val selectedCourtName = spinnerCourt.selectedItem as String
-            val selectedCourt = courts.firstOrNull { it.name == selectedCourtName }
-            val selectedCourtId = selectedCourt?.id ?: return@setOnClickListener
-
-            val selectedTimeString = spinnerTime.selectedItem as String
-            val organizerId = auth.currentUser?.uid ?: return@setOnClickListener
-
-
-            val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-            val selectedTime = timeFormat.parse(selectedTimeString)
-
-            // Get today's date
-            val today = Calendar.getInstance()
-
-            // Set the time to the selected time
-            val selectedTimeCalendar = Calendar.getInstance().apply {
-                time = selectedTime
-            }
-
-            today.set(Calendar.HOUR_OF_DAY, selectedTimeCalendar.get(Calendar.HOUR_OF_DAY))
-            today.set(Calendar.MINUTE, selectedTimeCalendar.get(Calendar.MINUTE))
-            today.set(Calendar.SECOND, 0)
-            today.set(Calendar.MILLISECOND, 0)
-
-            val timestamp = Timestamp(Date(today.timeInMillis))
-
-            val newMatch = hashMapOf(
-                "courtId" to selectedCourtId,
-                "dateTime" to timestamp, // You can adjust this to your needs
-                "organizerId" to organizerId,
-                "playerList" to listOf<String>() // Initially empty
-            )
-
-            db.collection("Matches")
-                .add(newMatch)
-                .addOnSuccessListener {
-                    Toast.makeText(this, "Match created successfully", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener { e ->
-                    Toast.makeText(this, "Error creating match: ${e.message}", Toast.LENGTH_SHORT).show()
-                }
+        recyclerView = findViewById(R.id.recyclerViewCourts)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        fetchReservedTimes {
+            setupCourtRecyclerView()
         }
 
 
+
+
     }
 
+    private fun fetchReservedTimes(completion: () -> Unit) {
+        db.collection("reserved_courts").get().addOnSuccessListener { documents ->
+            val format = SimpleDateFormat("HH:mm", Locale.getDefault())
+            format.timeZone = TimeZone.getTimeZone("UTC+1")
 
-    private fun setupCourtSpinner() {
+            reservedTimesMap.clear()
+            for (document in documents) {
+                val courtId = document.getString("courtId") ?: continue
+                val startTime = document.getTimestamp("startTime")?.toDate().toString() // Adjust date formatting as needed
+                val endTime = document.getTimestamp("endTime")?.toDate().toString() // Adjust date formatting as needed
+
+                // Assuming your availableTimes are in the same format
+                val times = reservedTimesMap.getOrPut(courtId) { mutableListOf() }.toMutableList()
+                times += startTime
+                times += endTime
+            }
+            completion()
+        }.addOnFailureListener { e ->
+            Toast.makeText(this, "Error fetching reserved times: ${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupCourtRecyclerView() {
         db.collection("courts")
             .get()
             .addOnSuccessListener { documents ->
-                courts = documents.toObjects(Court::class.java)
-                val adapter = ArrayAdapter(
-                    this,
-                    android.R.layout.simple_spinner_item,
-                    courts.map { it.name }
-                )
-                spinnerCourt.adapter = adapter
-
-                spinnerCourt.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                        val court = courts[position]
-                        setupTimeSpinner(court.availableTimes)
-                    }
-
-                    override fun onNothingSelected(parent: AdapterView<*>?) {}
+                val courts = documents.toObjects(Court::class.java)
+                createAdapter = CreateAdapter(courts, reservedTimesMap) { courtId, time ->
+                    createMatch(courtId, time)
                 }
+                recyclerView.adapter = createAdapter
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error loading courts: ${e.message}", Toast.LENGTH_SHORT).show()
             }
     }
 
-    private fun setupTimeSpinner(availableTimes: List<String>) {
-        val adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_spinner_item,
-            availableTimes
-        )
-        spinnerTime.adapter = adapter
+    private fun createMatch(courtId: String, time: String) {
+        val organizerId = auth.currentUser?.uid ?: return
+        val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault()).apply {
+            timeZone = TimeZone.getTimeZone("UTC+1")
+        }
+        val selectedTime = timeFormat.parse(time) ?: return
+        val today = Calendar.getInstance(TimeZone.getTimeZone("UTC+1")).apply {
+            set(Calendar.HOUR_OF_DAY, selectedTime.hours)
+            set(Calendar.MINUTE, selectedTime.minutes)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+
+
+
+        val endTime = Calendar.getInstance(TimeZone.getTimeZone("UTC+1")).apply {
+            timeInMillis = today.timeInMillis
+            add(Calendar.MINUTE, 90)
+        }
+
+        val startTimestamp = Timestamp(today.time)
+        val endTimestamp = Timestamp(endTime.time)
+
+
+
+        db.collection("reserved_courts")
+            .whereEqualTo("courtId", courtId)
+            .get()
+            .addOnSuccessListener { documents ->
+                val conflict = documents.any { document ->
+                    val existingStartTime = document.getTimestamp("startTime")?.toDate()
+                    val existingEndTime = document.getTimestamp("endTime")?.toDate()
+                    existingStartTime != null && existingEndTime != null &&
+                            !today.time.after(existingEndTime) && !endTime.time.before(existingStartTime)
+                }
+
+                if (conflict) {
+                    // Inform the user about the conflict and do not proceed with the creation
+                    Toast.makeText(this, "Selected time slot is already reserved.", Toast.LENGTH_LONG).show()
+                } else {
+                    // Proceed with creating the match as no conflict exists
+                    val matchTimestamp = Timestamp(today.time)
+                    val endMatchTimestamp = Timestamp(endTime.time)
+
+                    val newMatch = hashMapOf(
+                        "courtId" to courtId,
+                        "dateTime" to matchTimestamp,
+                        "organizerId" to organizerId,
+                        "playerList" to listOf<String>()
+                    )
+
+                    db.collection("Matches").add(newMatch)
+                        .addOnSuccessListener { documentReference ->
+                            val reservedCourt = hashMapOf(
+                                "courtId" to courtId,
+                                "courtName" to "Court Name Here", // Replace with actual court name
+                                "startTime" to startTimestamp,
+                                "endTime" to endTimestamp,
+                                "userId" to organizerId
+                            )
+                            db.collection("reserved_courts").add(reservedCourt)
+                                .addOnSuccessListener {
+                                    Toast.makeText(this, "Match and reservation created successfully", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener { e ->
+                                    Toast.makeText(this, "Error creating reservation: ${e.message}", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                        .addOnFailureListener { e ->
+                            Toast.makeText(this, "Error creating match: ${e.message}", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to check existing bookings.", Toast.LENGTH_SHORT).show()
+            }
+
+
+       /* db.collection("Matches").add(newMatch)
+            .addOnSuccessListener {
+                    documentReference ->
+                // If the match is successfully created, add a reservation to the reserved_courts collection
+                val reservedCourt = hashMapOf(
+                    "courtId" to courtId,
+                    // You'll need to provide the court name here, you can fetch it based on the courtId
+                    "courtName" to "Court Name Here",
+                    "startTime" to timestamp,
+                    // Calculate the end time based on your match duration
+                    "endTime" to Timestamp(Date(timestamp.seconds * 1000 + 5400000)),
+                    "userId" to organizerId
+                )
+                db.collection("reserved_courts").add(reservedCourt)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Match and reservation created successfully", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener { e ->
+                        Toast.makeText(this, "Error creating reservation: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error creating match: ${e.message}", Toast.LENGTH_SHORT).show()
+            }*/
+
     }
+
+
+
 
 }
